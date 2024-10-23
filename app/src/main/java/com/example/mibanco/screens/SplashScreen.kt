7ec59a4e6f8bada9012396b40a1e389e5b4f0fda
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-10-22 23:34:15 -0300

    agrego imagen

--- app/src/main/java/com/example/mibanco/screens/SplashScreen.kt
+++ app/src/main/java/com/example/mibanco/screens/SplashScreen.kt
@@ -1,5 +1,7 @@
 package com.example.mibanco.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,17 +9,26 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.mibanco.R
 
 @Composable
 fun SplashScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Spacer(modifier = Modifier.height(30.dp))
+        Image(
+            painter = painterResource(id = R.drawable.bank),
+            contentDescription = "Logo"
+        )
+        Spacer(modifier = Modifier.height(20.dp))
         Text(text = "Bienvenido a Mi Banco")
     }
 }

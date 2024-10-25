2d50a9af577d1724cb2182a7433b8ed901f73b06
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-10-24 22:06:29 -0300

    agrego enlaces menu

--- app/src/main/java/com/example/mibanco/components/MenuLateral.kt
+++ app/src/main/java/com/example/mibanco/components/MenuLateral.kt
@@ -1,14 +1,17 @@
 package com.example.mibanco.components
 
 import androidx.compose.material3.DrawerState
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.NavHostController
 import com.example.mibanco.models.ItemsMenuLateral.*
 import com.example.mibanco.navigation.currentRoute
+import kotlinx.coroutines.launch
 
 @Composable
 fun MenuLateral(
@@ -16,6 +19,7 @@ fun MenuLateral(
     drawerState: DrawerState,
     contenido: @Composable () -> Unit
 ) {
+    val scope = rememberCoroutineScope()
     val menuItems = listOf(
         ItemMenuLateral1,
         ItemMenuLateral2,
@@ -28,9 +32,17 @@ fun MenuLateral(
             ModalDrawerSheet {
                 menuItems.forEach { item ->
                     NavigationDrawerItem(
+                        icon = {
+                            Icon(item.icon, null)
+                        },
                         label = { Text(text = item.title) },
                         selected = currentRoute(navController) == item.ruta,
-                        onClick = {}
+                        onClick = {
+                            scope.launch {
+                                drawerState.close()
+                            }
+                            navController.navigate(item.ruta)
+                        }
                     )
                 }
             }
